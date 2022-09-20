a796d310d9e0baeef80dfe3c8b1f5e47c1a7bf1b
kyh <kyh_9429@naver.com>
2022-09-20 12:40:14 +0900

    movierecomm

---
+++ src/main/java/com/example/movierecomm/MovierecommApplication.java
@@ -0,0 +1,13 @@
+package com.example.movierecomm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovierecommApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovierecommApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/movierecomm/MovierecommApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.movierecomm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovierecommApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
