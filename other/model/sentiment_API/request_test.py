c04911378ea084605d93f285a223ab79d7427fd8
askenjade <csw02442@gmail.com>
2022-10-07 20:44:28 +0900

    build test

--- other/model/sentiment_API/request_test.py
+++ other/model/sentiment_API/request_test.py
@@ -5,7 +5,7 @@ import numpy as np
 
 url = 'http://localhost:8080/sentiment/predict'
 
-seperated_review = pd.read_csv('../outputs/split0.csv', encoding='utf-8')
+seperated_review = pd.read_csv('../../data/split0.csv', encoding='utf-8')
 df = seperated_review.iloc[:200]
 texts = df["document"].tolist()
 label = df["label"].tolist()
@@ -14,7 +14,11 @@ data = {'review_texts': texts}
 headers = {'Content-type': 'application/json'}
 response = requests.post(url, data=json.dumps(data), headers=headers)
 
-print("Accuracy: ", np.mean(np.array(label) == np.array(response.json())))
+result = response.json()
+print(result)
+print(label)
+print('Accuracy: ', np.mean(np.array(result) == np.array(label)))
 
 print(response.json())
+
 
--- other/model/sentiment_API/sentiment_Api.py
+++ other/model/sentiment_API/sentiment_Api.py
@@ -27,3 +27,4 @@ def predict():
 if __name__ == '__main__':
     app.run(port=8080)
 
+
